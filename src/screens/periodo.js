f045eeb837c02dda479ce17f840808c1ff54c1d4
Gustavo Venegas <150144979+GusVenegasS@users.noreply.github.com>
2024-12-02 20:25:07 -0500

    finalizar periodo, descargar archivo

--- src/screens/periodo.js
+++ src/screens/periodo.js
@@ -1,8 +1,13 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, ScrollView, TextInput, Button, StyleSheet, TouchableOpacity, TouchableWithoutFeedback, Keyboard } from 'react-native';
+import {
+    View, Text, ScrollView, RefreshControl, TextInput, Button, StyleSheet, TouchableOpacity,
+    TouchableWithoutFeedback, Keyboard, Modal, Alert
+} from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
 import { Calendar } from 'react-native-calendars';
+import TextStyles from '../styles/texto';
 import API from "../services/api-services";
+import LottieView from 'lottie-react-native';
 
 export default function VerificarPeriodo() {
     const [status, setStatus] = useState(null);
@@ -11,25 +16,31 @@ export default function VerificarPeriodo() {
     const [isCalendarVisible, setCalendarVisible] = useState(false);
     const [currentDateField, setCurrentDateField] = useState('');
     const [refresh, setRefresh] = useState(false);
+    const [mensaje, setMensaje] = useState('');
     const [data, setData] = useState(null);
+    const [modalVisible, setModalVisible] = useState(false);
+    const [modalAnimation, setModalAnimation] = useState(null);
+    const [isError, setIsError] = useState(false);
     const periodo = "2024-B";
 
     // Verificar el periodo al iniciar el componente
     const verificar = async () => {
         try {
             const response = await API.verificarPeriodo(periodo);
+            console.log(response)
             if (response.status === 200) {
-                setStatus('valido');
+                setStatus('valido');  // Estado 200: periodo válido
                 setData(response.data);
             } else if (response.status === 400) {
-                setStatus('activo');
+                setStatus('activo');  // Estado 400: estado activo
                 setData(response.data);
             } else if (response.status === 410) {
-                setStatus('finalizado');
+                setStatus('finalizado');  // Estado 410: estado finalizado
                 setData(response.data);
             }
         } catch (error) {
             console.error(error);
+            setMensaje("Upss! Algo salió mal")
             setStatus('error');
         }
     };
@@ -54,89 +65,245 @@ export default function VerificarPeriodo() {
     };
 
     const toggleCalendar = (field) => {
-        setCurrentDateField(field);
-        setCalendarVisible(prev => !prev || currentDateField !== field);
+        if (currentDateField === field && isCalendarVisible) {
+            setCalendarVisible(false);
+        } else {
+            setCurrentDateField(field);
+            setCalendarVisible(true);
+        }
     };
 
-    const crear = async () => {
-        console.log("Clic en crear", { fechaInicio, fechaFin });
-        setRefresh(prev => !prev);
-    };
+    const crear = async (fechaInicio, fechaFin) => {
+        try {
+            const respuestaCrear = await API.crearPeriodo(fechaInicio, fechaFin, periodo);
+            setMensaje(respuestaCrear.message);
 
-    const descargar = async () => {
-        console.log("Clic en descargar");
-        setRefresh(prev => !prev);
+            if (respuestaCrear.status === 200) {
+                setModalAnimation(require('../assets/animaciones/check.json'));
+                setIsError(false);
+                setModalVisible(true);
+                setTimeout(() => {
+                    setRefresh(!refresh);
+                    setModalVisible(false);
+                }, 3000); // Duración de la animación en milisegundos
+            } else {
+                setModalAnimation(require('../assets/animaciones/errorPerro.json'))
+                setIsError(true);
+                setModalVisible(true);
+            }
+        } catch (error) {
+            console.log(error);
+            setMensaje('Error al crear el periodo');
+            setModalAnimation(require('../assets/animaciones/error_500.json'));
+            setIsError(true);
+            setModalVisible(true);
+        }
     };
 
     const finalizar = async () => {
-        console.log("Clic en finalizar");
-        setRefresh(prev => !prev);
+        try {
+            const respuestaFinalizar = await API.finalizarPeriodo(periodo);
+            setMensaje(respuestaFinalizar.message);
+            if (respuestaFinalizar.status === 200) {
+                setModalAnimation(require('../assets/animaciones/check.json'));
+                setIsError(false);
+                setModalVisible(true);
+                setTimeout(() => {
+                    setRefresh(!refresh);
+                    setModalVisible(false);
+                }, 3000); // Duración de la animación en milisegundos
+            } else {
+                setModalAnimation(require('../assets/animaciones/errorPerro.json'))
+                setIsError(true);
+                setModalVisible(true);
+            }
+        } catch (error) {
+            console.error(error);
+            setMensaje('Error al finalizar el periodo');
+            setModalAnimation(require('../assets/animaciones/error_500.json'));
+            setIsError(true);
+            setModalVisible(true);
+        }
     };
 
-    const renderCalendars = (dateField) => (
-        isCalendarVisible && currentDateField === dateField && (
-            <View style={styles.calendarContainer}>
-                <Calendar
-                    onDayPress={handleDateSelect}
-                    markedDates={{ [dateField === 'fechaInicio' ? fechaInicio : fechaFin]: { selected: true, selectedColor: 'blue' } }}
-                    theme={{ selectedDayBackgroundColor: '#007BFF', selectedDayTextColor: '#ffffff', todayTextColor: '#007BFF' }}
-                />
-            </View>
-        )
-    );
+    const descargar = async () => {
+        try {
+            const path = await API.descargarReporte(periodo);
+            console.log(path)// Usar la función que descargará el archivo
+            Alert.alert('Descarga completa', `El archivo se ha descargado en: ${path}`);
+        } catch (error) {
+            console.log(error);
+            Alert.alert('Error', 'No se pudo descargar el archivo.');
+        }
+    };
 
-    const renderInputField = (label, value, field) => (
-        <View style={styles.inputContainer}>
-            <Text style={styles.label}>{label}:</Text>
-            <View style={styles.inputWrapper}>
-                <TextInput
-                    style={styles.input}
-                    placeholder="AAAA-MM-DD"
-                    value={value}
-                    editable={false}
-                />
-                <TouchableOpacity onPress={() => toggleCalendar(field)}>
-                    <Icon name="calendar" size={20} />
-                </TouchableOpacity>
+    // Componente de campo de fecha reutilizable
+    const FechaInput = ({ label, value, editable, onPress, calendarVisible }) => {
+        return (
+            <View style={styles.inputContainer}>
+                <Text style={styles.label}>{label}</Text>
+                <View style={styles.inputWrapper}>
+                    <TextInput
+                        style={styles.input}
+                        value={value}
+                        editable={editable}
+                    />
+                    {editable && (
+                        <TouchableOpacity onPress={onPress}>
+                            <Icon name="calendar" size={20} />
+                        </TouchableOpacity>
+                    )}
+                </View>
+                {calendarVisible && (
+                    <View style={styles.calendarContainer}>
+                        <Calendar
+                            onDayPress={handleDateSelect}
+                            markedDates={{
+                                [value]: { selected: true, selectedColor: 'blue' },
+                            }}
+                            theme={{
+                                selectedDayBackgroundColor: '#007BFF',
+                                selectedDayTextColor: '#ffffff',
+                                todayTextColor: '#007BFF',
+                            }}
+                        />
+                    </View>
+                )}
             </View>
-            {renderCalendars(field)}
-        </View>
-    );
-
-    const renderButton = (title, onPress) => (
-        <View style={styles.buttonContainer}>
-            <Button title={title} onPress={onPress} color="#007BFF" />
-        </View>
-    );
-
-    const renderCard = (extraContent) => (
-        <View style={styles.container}>
-            <View style={styles.card}>
-                {renderInputField("Fecha Inicio", data?.fechaInicio || fechaInicio, 'fechaInicio')}
-                {renderInputField("Fecha Fin", data?.fechaFin || fechaFin, 'fechaFin')}
-                {extraContent}
-            </View>
-        </View>
-    );
+        );
+    };
 
     return (
         <ScrollView>
-            {status === 'valido' && data && (
+            <Modal
+                animationType="slide"
+                transparent={true}
+                visible={modalVisible}
+                onRequestClose={() => {
+                    setModalVisible(!modalVisible);
+                }}
+            >
+                <View style={styles.centeredView}>
+                    <View style={styles.modalView}>
+                        <LottieView
+                            source={modalAnimation}
+                            autoPlay
+                            loop={!isError}
+                            style={styles.lottie}
+                        />
+                        <Text style={styles.modalText}>{mensaje}</Text>
+                        {isError && (
+                            <TouchableOpacity
+                                style={styles.closeButton}
+                                onPress={() => setModalVisible(false)}
+                            >
+                                <Icon name="x" size={30} color="black" />
+                            </TouchableOpacity>
+                        )}
+                    </View>
+                </View>
+            </Modal>
+            {status === 'valido' && data != null && (
                 <TouchableWithoutFeedback onPress={handleOutsidePress}>
-                    {renderCard(renderButton("Crear", crear))}
+                    <View style={styles.container}>
+                        <View style={styles.card}>
+                            {/* Renderizar los campos de fecha solo cuando no están en estado 'activo' o 'finalizado' */}
+                            <FechaInput
+                                label="Fecha Inicio:"
+                                value={fechaInicio}
+                                editable={true}
+                                onPress={() => toggleCalendar('fechaInicio')}
+                                calendarVisible={isCalendarVisible && currentDateField === 'fechaInicio'}
+                            />
+                            <FechaInput
+                                label="Fecha Fin:"
+                                value={fechaFin}
+                                editable={true}
+                                onPress={() => toggleCalendar('fechaFin')}
+                                calendarVisible={isCalendarVisible && currentDateField === 'fechaFin'}
+                            />
+                            <View style={styles.buttonContainer}>
+                                <Button
+                                    title="Crear"
+                                    onPress={() => crear(fechaInicio, fechaFin)}
+                                    color="#007BFF"
+                                />
+                            </View>
+                        </View>
+                    </View>
                 </TouchableWithoutFeedback>
             )}
-            {status === 'activo' && data && renderCard(renderButton("Finalizar", finalizar))}
-            {status === 'finalizado' && data && renderCard(
-                <>
-                    {renderInputField("Fecha Finalización", data.fechaFinalización, 'fechaFinalización')}
-                    <Text style={styles.label}>Este período ya fue finalizado, puedes descargar el reporte</Text>
-                    {renderButton("Descargar reporte", descargar)}
-                </>
+            {status === 'activo' && data != null && (
+                <View style={styles.container}>
+                    <View style={styles.card}>
+                        <FechaInput
+                            label="Fecha Inicio:"
+                            value={data.fechaInicio}
+                            editable={false}
+                            onPress={() => { }}
+                            calendarVisible={false}
+                        />
+                        <FechaInput
+                            label="Fecha Fin:"
+                            value={data.fechaFin}
+                            editable={false}
+                            onPress={() => { }}
+                            calendarVisible={false}
+                        />
+                        <View style={styles.buttonContainer}>
+                            <Button
+                                title="Finalizar"
+                                onPress={finalizar}
+                                color="#007BFF"
+                            />
+                        </View>
+                    </View>
+                </View>
+            )}
+            {status === 'finalizado' && data != null && (
+                <View style={styles.container}>
+                    <View style={styles.card}>
+                        <FechaInput
+                            label="Fecha Inicio:"
+                            value={data.fechaInicio}
+                            editable={false}
+                            onPress={() => { }}
+                            calendarVisible={false}
+                        />
+                        <FechaInput
+                            label="Fecha Fin:"
+                            value={data.fechaFin}
+                            editable={false}
+                            onPress={() => { }}
+                            calendarVisible={false}
+                        />
+                        <FechaInput
+                            label="Fecha Finalización:"
+                            value={data.fechaFinalización}
+                            editable={false}
+                            onPress={() => { }}
+                            calendarVisible={false}
+                        />
+                        <Text style={styles.label}>Este período ya fue finalizado, puedes descargar el reporte</Text>
+                        <View style={styles.buttonContainer}>
+                            <Button
+                                title="Descargar reporte"
+                                onPress={descargar}
+                                color="#007BFF"
+                            />
+                        </View>
+                    </View>
+                </View>
             )}
             {status === 'error' && (
-                <View>
-                    <Text>Algo salió mal</Text>
+                <View style={styles.centeredView}>
+                    <LottieView
+                        source={require('../assets/animaciones/serverError.json')}
+                        autoPlay
+                        loop
+                        style={styles.lottie}
+                    />
+                    <Text style={styles.modalText}>{mensaje}</Text>
                 </View>
             )}
         </ScrollView>
@@ -146,59 +313,95 @@ export default function VerificarPeriodo() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'flex-start',
-        padding: 16,
+        padding: 20,
     },
     card: {
-        padding: 16,
-        marginVertical: 8,
         backgroundColor: '#fff',
-        borderWidth: 1,
-        borderColor: '#008EB6',
-        borderRadius: 8,
+        padding: 20,
+        borderRadius: 10,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.1,
+        shadowRadius: 5,
+        elevation: 5,
     },
     inputContainer: {
         marginBottom: 20,
-        position: 'relative',
     },
     label: {
-        marginBottom: 5,
         fontSize: 16,
-        color: '#333',
+        marginBottom: 8,
     },
     inputWrapper: {
         flexDirection: 'row',
         alignItems: 'center',
-        borderColor: '#ccc',
-        borderWidth: 1,
-        borderRadius: 5,
-        paddingHorizontal: 10,
     },
     input: {
         flex: 1,
-        height: 40,
-        color: '#000',
-    },
-    buttonContainer: {
-        marginTop: 10,
-        borderRadius: 5,
-        overflow: 'hidden',
-    },
-    calendarContainer: {
-        marginTop: 10,
-        backgroundColor: 'white',
-        zIndex: 100,
-        position: 'absolute',
-        top: 50,
-        left: 0,
-        right: 0,
+        padding: 10,
         borderWidth: 1,
         borderColor: '#ccc',
         borderRadius: 5,
-        shadowColor: '#000',
-        shadowOffset: { width: 0, height: 2 },
+        marginRight: 10,
+    },
+    calendarContainer: {
+        marginTop: 10,
+        position: 'absolute',
+        zIndex: 10,
+        backgroundColor: 'white',
+        borderRadius: 10,
+        borderWidth: 1,
+        borderColor: '#ccc',
+    },
+    buttonContainer: {
+        marginTop: 20,
+    },
+    centeredView: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: 22,
+    },
+    modalView: {
+        margin: 20,
+        backgroundColor: "white",
+        borderRadius: 20,
+        padding: 35,
+        alignItems: "center",
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
         shadowOpacity: 0.25,
-        shadowRadius: 3.84,
+        shadowRadius: 4,
         elevation: 5,
+    },
+    modalText: {
+        marginBottom: 15,
+        textAlign: "center",
+    },
+    lottie: {
+        width: 150,
+        height: 150,
+    },
+    button: {
+        padding: 10,
+        marginTop: 20,
+        backgroundColor: '#007BFF',
+        borderRadius: 5,
+    },
+    buttonClose: {
+        backgroundColor: '#FF3B30',
+    },
+    textStyle: {
+        color: 'white',
+        textAlign: 'center',
+    },
+    closeButton: {
+        position: 'absolute',
+        top: 10,
+        right: 10,
+        padding: 5,
     },
 });
