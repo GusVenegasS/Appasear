32c4c24ad669b8ce3b13dab11568cc26b944dc88
RonGonzalez01 <ronnie.gon12@gmail.com>
2024-12-02 20:29:18 -0500

    Listo para unir front

--- src/config/routes.js
+++ src/config/routes.js
@@ -3,6 +3,17 @@ import Brigadas from '../screens/brigadas';
 import Usuarios from '../screens/usuarios';
 import Configuracion from '../screens/configuraciones';
 import LoginScreen from '../screens/LoginScreen';
+<<<<<<< Updated upstream
+=======
+import HomeStudent from '../screens/HomeStudent';
+import BrigadasStudent from '../screens/BrigadasStudent';
+import ConfiguracionStudent from '../screens/ConfiguracionStudent';
+import HomeStack from '../screens/HomeStack';
+import Usuario from '../screens/usuario';
+import Periodo from '../screens/periodo';
+import Tarea from '../screens/detalleTarea';
+import Perfil from '../screens/PerfilScreen';
+>>>>>>> Stashed changes
 
 export const screens = [
   {
@@ -29,4 +40,76 @@ export const screens = [
     },
     rol: 'admin'
   },
+<<<<<<< Updated upstream
+=======
+  {
+    name: 'Información estudiante',
+    component: Usuario,
+    options: {
+      tabBarButton: () => null, tabBarIcon: 'info'
+    },
+    rol: 'admin'
+  },
+  {
+    name: 'Período académico',
+    component: Periodo,
+    options: {
+      tabBarButton: () => null, tabBarIcon: 'info'
+    },
+    rol: 'admin'
+  },
+  {
+    name: 'Perfil',
+    component: Perfil,
+    rol: 'admin'
+  },
+/*
+  {
+    name: 'Home',
+    component: HomeStudent,
+    options: {
+      tabBarIcon: 'home-outline', // Nombre del icono home student
+    },
+    rol: 'user'
+  }*/
+ {
+  name: 'Tareas',
+  component: HomeStack,
+  options: {
+    tabBarIcon: 'home-outline',
+  },
+  rol: 'user'
+ },
+  {
+    name: 'Brigadas',
+    component: BrigadasStudent,
+    options: {
+      tabBarIcon: 'person-outline', // Nombre del icono home student
+    },
+    rol: 'user'
+  },
+  {
+    name: 'Configuración',
+    component: ConfiguracionStudent,
+    options: {
+      tabBarIcon: 'settings-outline', // Nombre del icono home student
+    },
+    rol: 'user'
+  },
+
+  {
+    name: 'Detalle tarea',
+    component: Tarea,
+    options: {
+      tabBarButton: () => null, tabBarIcon: 'info'
+    },
+    rol: 'admin'
+  },
+
+  {
+    name: 'Perfil',
+    component: Perfil,
+    rol: 'user'
+  },
+>>>>>>> Stashed changes
 ];
---
+++ src/services/api-services.js
@@ -0,0 +1,91 @@
+function crearPeriodo(fechaInicio, fechaFin, periodo) {
+    let b = {
+        fechaInicio: fechaInicio,
+        fechaFin: fechaFin,
+        periodoAcademico: periodo
+    }
+    let url = `http://192.168.100.3:50000/crearPeriodo`;
+    const requestOptions = {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(b)
+    };
+
+    return new Promise((resolve, reject) => {
+        fetch(url, requestOptions)
+            .then((res) => res.json())
+            .then(
+                (respuesta) => {
+                    resolve(respuesta);
+                },
+                (error) => reject(error)
+            );
+    });
+}
+
+function obtenerBrigadas(periodo) {
+    console.log("bodyBrigadas: " + periodo);
+    let url = `http://192.168.100.3:50000/obtenerBrigadas?periodoAcademico=${periodo}`;
+    const requestOptions = {
+        method: "GET",
+        headers: { "Content-Type": "application/json" },
+    };
+
+    return new Promise((resolve, reject) => {
+        fetch(url, requestOptions)
+            .then((res) => res.json())
+            .then(
+                (respuesta) => {
+                    resolve(respuesta);
+                },
+                (error) => reject(error)
+            );
+    });
+}
+
+function obtenerUsuarios(periodo) {
+    console.log("body: " + periodo);
+    let url = `http://192.168.100.3:50000/usuarios?periodoAcademico=${periodo}`;
+    const requestOptions = {
+        method: "GET",
+        headers: { "Content-Type": "application/json" },
+    };
+
+    return new Promise((resolve, reject) => {
+        fetch(url, requestOptions)
+            .then((res) => res.json())
+            .then(
+                (respuesta) => {
+                    resolve(respuesta);
+                },
+                (error) => reject(error)
+            );
+    });
+}
+
+function verTarea(fecha, brigadaID, periodo) {
+    console.log("body: " + fecha);
+    let url = `http://192.168.100.3:50000/verTarea?periodoAcademico=${periodo}&fechaQuery=${fecha}&brigada_id=${brigadaID}`;
+    const requestOptions = {
+        method: "GET",
+        headers: { "Content-Type": "application/json" },
+    };
+
+    return new Promise((resolve, reject) => {
+        fetch(url, requestOptions)
+            .then((res) => res.json())
+            .then(
+                (respuesta) => {
+                    resolve(respuesta);
+                },
+                (error) => reject(error)
+            );
+    });
+}
+
+export default {
+    crearPeriodo,
+    obtenerBrigadas,
+    obtenerUsuarios,
+    verTarea
+}
