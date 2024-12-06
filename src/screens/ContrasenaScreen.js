fe09ef6917b40edb2aff2871a8d966b16c11ee33
RonGonzalez01 <ronnie.gon12@gmail.com>
2024-12-05 20:34:02 -0500

    Create ContrasenaScreen.js

---
+++ src/screens/ContrasenaScreen.js
@@ -0,0 +1,93 @@
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, Alert, ScrollView } from 'react-native';
+import axios from 'axios';
+import textStyles from '../styles/texto';
+
+const ContrasenaScreen = ({ navigation }) => {
+
+  return (
+    <ScrollView contentContainerStyle={styles.scrollContainer}>
+      <View style={styles.container}>
+        <View style={styles.formContainer}>
+
+          <Text style={textStyles.title3}>Correo Electrónico</Text>
+          <View style={styles.inputContainer}>
+            <TextInput
+              style={textStyles.cuerpo}
+              value={correo}
+              onChangeText={setCorreo}
+              placeholder="Ingresa el correo electrónico"
+              keyboardType="email-address"
+            />
+          </View>
+
+          <TouchableOpacity style={styles.button} onPress={handleAddStudent}>
+            <Text style={styles.buttonText}>Agregar Estudiante</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </ScrollView>
+  );
+};
+
+const styles = {
+  container: {
+    flex: 1,
+    padding: 1,
+    justifyContent: 'center',
+  },
+  scrollContainer: {
+    paddingHorizontal: 30,
+    paddingVertical: 20,
+    flexGrow: 1,
+    justifyContent: 'center',
+  },
+  inputContainer: {
+    marginBottom: 20,
+    paddingHorizontal: 16,
+    borderWidth: 1,
+    borderColor: 'black',
+    borderRadius: 10,
+    backgroundColor: 'white',
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 8,
+    paddingLeft: 10,
+    paddingRight: 40,
+    height: 50,
+    fontSize: 16,
+  },
+  button: {
+    backgroundColor: "#008EB6",
+    borderRadius: 10,
+    paddingVertical: 15,
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  label: {
+    fontSize: 14,
+    color: '#555',
+    marginBottom: 5,
+  },
+  formContainer: {
+    backgroundColor: 'white',
+    borderRadius: 10,
+    borderWidth: 2,
+    borderColor: '#008EB6',
+    padding: 20,
+    marginHorizontal: 10,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 3,
+    elevation: 3,
+  },
+};
+
+export default ContrasenaScreen;
